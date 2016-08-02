65f52541c5e18125c27056d36185a85dd7bd3cf8
Zahari <zaharid@gmail.com>
2016-08-02 13:18:49 +0200

    Add README

---
+++ README.md
@@ -0,0 +1,22 @@
+Asyncwatch
+=========
+
+An asyncronous
+[inotify](http://man7.org/linux/man-pages/man7/inotify.7.html) wrapper based on curio.
+
+
+Installing
+-----------
+
+````
+pip install asyncwatch
+````
+
+Requirements
+-----------
+
+- A recent enough version of the Linux Kernel (at least 2.7).
+- Python >= 3.5
+- [Curio](https://github.com/dabeaz/curio)
+
+
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 
 setup(
     name="asyncwatch",
     version='0.1',
     description="An asyncronous inotify wrapper based on curio",
+    long_description = long_description,
     setup_requires=["cffi>=1.0"],
     cffi_modules=["asyncwatch/ffibuilder.py:I"],
     install_requires=["cffi>=1.0", "curio"],
